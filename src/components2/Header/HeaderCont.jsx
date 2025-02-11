7b14060c0cd7f39b4d25310fbb4891fbc8501452
jeisgar13 <jeisgar13@gmail.com>
2025-02-10 16:06:58 -0800

    [ApiKey] - Update apiKey and response.

--- src/components2/Header/HeaderCont.jsx
+++ src/components2/Header/HeaderCont.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './headercont.css';
 import weatherLogo from '../../pictures/weatherlogo.png';
 
-export const HeaderCont = () => {
+export const Header = () => {
 
     return(
         <div className='header-cont' style={{display: 'flex'}}>
