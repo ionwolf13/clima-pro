fa4c71bb8cd087143eebabfdd070d64d491a1bd3
Israel Garcia <isra@Mac.attlocal.net>
2025-05-18 20:18:39 -0700

    [Layout] - Fix layout and styling.

--- src/components/Forecast/CurrentHourlyForecast/CurrentHourlyForecast.tsx
+++ src/components/Forecast/CurrentHourlyForecast/CurrentHourlyForecast.tsx
@@ -5,8 +5,9 @@ export const CurrentHourlyForecast = () => {
   return (
     <div
       style={{
-        border: "2px solid black",
-        maxWidth: "800px",
+        // border: "2px solid black",
+        borderRadius: "8px",
+        maxWidth: "600px",
         width: "100%",
         maxHeight: "480px",
         height: "100%",
@@ -14,7 +15,7 @@ export const CurrentHourlyForecast = () => {
         flexDirection: "column",
         alignItems: "center",
         rowGap: "16px",
-        padding: "8px",
+        padding: "0px 8px",
         backgroundColor: " rgba(255, 255, 255, 0.2)"
       }}
     >
